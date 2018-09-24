80db75481beb0c5c9559612af9d1f958ca64eff1
Hugin Jacobsen <huginjacobsen@mac.com>
2018-09-24 21:40:34 +0100

    works now

--- xd.py
+++ xd.py
@@ -1,1 +1,24 @@
 print ("Hugin2ádev")
+
+def add(num1, num2):
+    return num1 + num2
+
+def minus(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+operation = input ("What would you like to do?")
+n1 = int(input("Choose a number:"))
+n2 = int(input("Choose a second number:"))
+
+if operation == "+":
+     result = add(n1, n2)
+elif operation == "-":
+    result = minus(n1, n2)
+
+print(result)
